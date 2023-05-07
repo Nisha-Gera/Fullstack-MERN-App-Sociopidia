1a413446a51be62dffca2bd5c13a379a3072c7de
Nisha gera <nishaa.1803gera@gmail.com>
2023-05-07 22:46:34 +0530

    2 commit
    Signed-off-by: Nisha gera <nishaa.1803gera@gmail.com>

--- Server/index.js
+++ Server/index.js
@@ -22,3 +22,5 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
+
+// 
